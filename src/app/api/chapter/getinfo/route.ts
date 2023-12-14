e381e5a15c5f0ac65b46bf4408767baa68d2789f
jun-hash <wnstjsy@korea.ac.kr>
2023-12-14 05:28:00 -0500

    :sparkles: feat(be) chapter youtube, summary 자동생성 api 구현

--- src/app/api/chapter/getinfo/route.ts
+++ src/app/api/chapter/getinfo/route.ts
@@ -3,15 +3,72 @@
 import { prisma } from "@/lib/db";
 import {
 
+  getTranscript,
   searchYoutube,
 } from "@/lib/actions/youtube";
 import { NextResponse } from "next/server";
 import { z } from "zod";
+import { strict_output } from "@/lib/actions/gpt";
 
+const bodyParser = z.object({
+  chapterId: z.string()
+})
 
 // flow chart
 
 // 1. find chapter (id) -> 2. chatpter.query -> get video(search and get => videoid)
-// => chapter update (videoid,summary)
-// => get transcript (chaper에 추가)
+export async function POST (req:Request, res:Response) {
+  try {
+    const body = await req.json()
+    const { chapterId } = bodyParser.parse(body)
+    const chapter = await prisma.chapter.findUnique({
+      where : {
+        id: chapterId,
+      }
+    })
+    if(!chapter) {
+      return NextResponse.json({success:false, error:"Chapter not found"}, {status:400})
+    }
+
+    const videoId = await searchYoutube(chapter.youtubeSearchQuery)
+    let transcript = await getTranscript(videoId)
+    let maxLength = 500;
+    transcript = transcript.split('').slice(0,maxLength).join("")
+
+    const { summary }: {summary:string} = await strict_output(
+      "You are an AI capable of summarising a youtube transcript",
+      "summarise in 250 words or less and do not talk of the sponsors or anything unrelated to the main topic, also do not introduce what the summary is about.\n" + transcript,
+      {summary: "summary of the transcript"}
+    )
+
+    await prisma.chapter.update({
+      where : {id:chapterId},
+      data  : {
+        videoId: videoId,
+        summary: summary,
+      }
+    })
+
+    return NextResponse({success:true}, {status:200})
+    
+  } catch (error) {
+      if(error instanceof z.ZodError) {
+        return NextResponse({
+          success:false,
+          error: "Invalid body"
+        },
+        {status: 400}
+        )
+      } else {
+        return NextResponse.json(
+          {
+          success: false,
+          error: "unknown"
+        },
+        {status : 500 }
+        )
+      }
+    
+  }
+}
 
